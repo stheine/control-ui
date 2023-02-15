8730eb27ffeb03504801c422dcf92f39348009f4
Stefan Heine <stheine@arcor.de>
2023-02-15 16:51:22 +0100

    Wetter, Vorhersage

--- src/components/Wetter/Wetter.jsx
+++ src/components/Wetter/Wetter.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {
   useContext,
   useEffect,
@@ -24,46 +25,81 @@ export default function Wetter() {
   }
 
   const wetter           = _message?.current.weather[0].description || '';
-  const temperatur       = _message?.current.temp || 99;
-  const gefuehlt         = _message?.current.feels_like || 99;
-  const bewoelkung       = _message?.current.clouds || 99;
-  const luftfeuchtigkeit = _message?.current.humidity || 0;
+  const temperatur       = _message?.current.temp       === undefined ? 99 : _message.current.temp;
+  const gefuehlt         = _message?.current.feels_like === undefined ? 99 : _message.current.feels_like;
+  const bewoelkung       = _message?.current.clouds     === undefined ? 99 : _message.current.clouds;
+  const luftfeuchtigkeit = _message?.current.humidity   === undefined ? 99 : _message.current.humidity;
   const warnungEvent     = _message?.alerts?.[0].event || '';
   // const warnungDesc      = _message?.alerts?.[0].description || '';
   // TODO Vorhersage
 
+  const nacht = function() {
+    return [
+      <tr key='nightLabel'>
+        <td colSpan={2} style={{fontWeight: 'bold'}}>Nacht</td>
+      </tr>,
+      <tr key='nightTemp'>
+        <td>Temperatur:</td>
+        <td style={{whiteSpace: 'nowrap'}}>
+          {_.round(_message?.nextNightMinTemp)}&deg; - {_.round(_message?.nextNightMaxTemp)}&deg;
+        </td>
+      </tr>,
+      <tr key='nightWind'>
+        <td>Max Wind:</td>
+        <td>{_.round(_message?.nextNightMaxWind)}&nbsp;<font style={{fontSize: '50%'}}>m/s</font></td>
+      </tr>,
+    ];
+  };
+
+  const tag = function() {
+    return [
+      <tr key='dayLabel'>
+        <td colSpan={2} style={{fontWeight: 'bold'}}>Tag</td>
+      </tr>,
+      <tr key='dayTemp'>
+        <td>Temperatur:</td>
+        <td style={{whiteSpace: 'nowrap'}}>
+          {_.round(_message?.nextDayMinTemp)}&deg; - {_.round(_message?.nextDayMaxTemp)}&deg;
+        </td>
+      </tr>,
+      <tr key='dayWind'>
+        <td>Max Wind:</td>
+        <td>{_.round(_message?.nextDayMaxWind)}&nbsp;<font style={{fontSize: '50%'}}>m/s</font></td>
+      </tr>,
+    ];
+  };
+
   return (
-    <table style={{padding: '0 30px 0 0'}}>
+    <table style={{fontSize: '150%', padding: '0 0 0 0'}}>
       <tbody>
         <tr>
           <td colSpan={2} style={{whiteSpace: 'nowrap'}}>{wetter}</td>
         </tr>
         <tr>
           <td>Bewölkung:</td>
-          <td style={{whiteSpace: 'nowrap'}}>{bewoelkung} %</td>
+          <td>{bewoelkung}&nbsp;%</td>
         </tr>
         <tr>
           <td>Temperatur:</td>
-          <td style={{whiteSpace: 'nowrap'}}>{temperatur} °C</td>
+          <td>{temperatur}&deg;</td>
         </tr>
         <tr>
           <td>Gefühlt:</td>
-          <td style={{whiteSpace: 'nowrap'}}>{gefuehlt} °C</td>
+          <td>{gefuehlt}&deg;</td>
         </tr>
         <tr>
           <td>Luftfeuchtigkeit:</td>
-          <td style={{whiteSpace: 'nowrap'}}>{luftfeuchtigkeit} %rH</td>
+          <td>{luftfeuchtigkeit}&nbsp;%</td>
         </tr>
         {warnungEvent ?
           <tr>
             <td colSpan={2} style={{backgroundColor: '#ff0000'}}>{warnungEvent}</td>
           </tr> :
           null}
+        {new Date().getHours() < 20 ?
+          [...tag(), ...nacht()] :
+          [...nacht(), ...tag()]}
       </tbody>
     </table>
   );
 }
-//        <tr>
-//          <td>Wechselrichter:</td>
-//          <td style={{whiteSpace: 'nowrap'}}>{`${_.round(wechselrichterErzeugung / 1000, 1)} kW`}</td>
-//        </tr>
