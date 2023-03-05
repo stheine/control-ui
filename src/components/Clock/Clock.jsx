c1419af4bef43ebee7b32e20b9c0514644406115
Stefan Heine <stheine@arcor.de>
2023-03-05 11:13:59 +0100

    Clock, fix updates, change font

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import ms from 'ms';
+import React, {
+  useState,
+} from 'react';
+
+// https://stackoverflow.com/a/10797177
+const repeatEvery = function(func, interval) {
+  // Check current time and calculate the delay until next interval
+  const now   = new Date();
+  const delay = interval - (now % interval);
+
+  const start = function() {
+    // Execute function now...
+    func();
+      // ... and every interval
+    setInterval(func, interval);
+  };
+
+  // Delay execution until it's an even interval
+  setTimeout(start, delay);
+};
 
 export default function Clock() {
-  const now = new Date();
+  const [_now, setNow] = useState(new Date());
+
+  repeatEvery(() => setNow(new Date()), ms('4s')); // TODO 1m
 
   return (
     <div className='clock'>
-      <div className='clock__time'>{now.getHours()}:{now.getMinutes()}</div>
-      <div className='clock__date'>{now.getDay()}.{now.getMonth() + 1}.{now.getFullYear()}</div>
+      <div className='clock__time'>
+        {_now.toLocaleTimeString('de-DE', {timeStyle: 'short'})}
+      </div>
+      <div className='clock__date'>
+        <div className='clock__date__day'>
+          {_now.toLocaleDateString('de-DE', {weekday: 'short'})}
+        </div>
+        <div className='clock__date__date'>
+          {_now.toLocaleDateString('de-DE')}
+        </div>
+      </div>
     </div>
   );
 }
